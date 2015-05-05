21e57739f2dbc46eb8d592300844441504473cc5
Diego Laga Ramon <diego.laga@gmail.com>
2015-05-05 23:28:57 +0100

    Jackson test
    Added some initial tests to play around with Dropwizard testing
    framework. The examples on their website are wrong.

---
+++ src/test/java/net/goodtwist/dev/grunt/test/jackson/UserAccountTest.java
@@ -0,0 +1,37 @@
+package net.goodtwist.dev.grunt.test.jackson;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.*;
+import io.dropwizard.jackson.Jackson;
+import net.goodtwist.dev.grunt.core.UserAccount;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UserAccountTest {
+
+    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+    @Test
+    public void serializesToJSON() throws Exception {
+        UserAccount userAccount = new UserAccount();
+        userAccount.setUsername("user_test");
+        userAccount.setPassword("pass_test");
+        userAccount.setEmail("email_test");
+
+        final String expected = MAPPER.writeValueAsString(MAPPER.readValue(fixture("fixtures/useraccount.json"), UserAccount.class));
+
+        assertThat(MAPPER.writeValueAsString(userAccount)).isEqualTo(expected);
+    }
+    
+    @Test
+    public void deserializesFromJSON() throws Exception {
+        UserAccount userAccount = new UserAccount();
+        userAccount.setUsername("user_test");
+        userAccount.setPassword("pass_test");
+        userAccount.setEmail("email_test");
+        
+        assertThat(MAPPER.readValue(fixture("fixtures/useraccount.json"), UserAccount.class)).isEqualToComparingFieldByField(userAccount);
+    }
+}
